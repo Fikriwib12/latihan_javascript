326ccbbf18019618e5b7428a518b4dc103592e48
unknown <ganarez03@gmail.com>
2023-11-15 13:02:05 +0700

    tambah darkmode toogle

--- belajar_tailwind2/tailwind.config.js
+++ belajar_tailwind2/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['index.html'],
+  darkMode:'class',
   theme: {
-    darkMode:'class',
     extend: {
       spacing: {
         13 : '3.25rem',
